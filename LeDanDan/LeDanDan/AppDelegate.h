c6fc80bbc5229a849bf35a5d36546769c3e3b127
herryhan <18001575779@163.com>
2015-11-16 15:23:35 +0800

    背景

---
+++ LeDanDan/LeDanDan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeDanDan
+//
+//  Created by yzx on 15/11/16.
+//  Copyright © 2015年 yzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeDanDan/LeDanDan/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  LeDanDan
+//
+//  Created by yzx on 15/11/16.
+//  Copyright © 2015年 yzx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
